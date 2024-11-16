b59d1d570483060c6aac30e5eaf988becf0f87ce
ente_qeq <nik_ryabinkin2001@mail.ru>
2024-11-16 19:01:37 +0500

    Практическое задание по теме "Переменные"

---
+++ variables.py
@@ -0,0 +1,5 @@
+number_of_completed_homework = 12 #Количество выполненных ДЗ
+number_of_hours_spent = 1.5       #Количество затраченных часов
+course_name = 'pythone'           #Название курса
+time_for_one_task = (number_of_hours_spent / number_of_completed_homework) #Время на одно задание
+print("курс", course_name, "кол-во выполненных дз", number_of_completed_homework, "кол-во затраченных часов" ,number_of_hours_spent, "время на одно задание", time_for_one_task)
